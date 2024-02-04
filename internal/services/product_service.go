6f0718dafcb37378cc40d8910f5bb418623ab227
Vinicius Pires <vinicius.pires@hotmart.com>
2024-02-04 15:49:47 -0300

    Add productService

---
+++ internal/services/product_service.go
@@ -0,0 +1,49 @@
+package services
+
+import (
+	"github.com/Viniciuscpires/fc_api_product/internal/database"
+	"github.com/Viniciuscpires/fc_api_product/internal/entity"
+)
+
+type ProductService struct {
+	ProductDB database.ProductDB
+}
+
+func NewProductService(db database.ProductDB) *ProductService {
+	return &ProductService{
+		ProductDB: db,
+	}
+}
+
+func (ps *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) GetProdutById(id string) (*entity.Product, error) {
+	product, err := ps.ProductDB.GetProduct(id)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (ps *ProductService) GetProdutByCategory(category_id string) ([]*entity.Product, error) {
+	products, err := ps.ProductDB.GetProductByCategory(category_id)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (ps *ProductService) CreateProduct(name, description, category_id, image_url string, price float64) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, category_id, image_url, price)
+	_, err := ps.ProductDB.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
